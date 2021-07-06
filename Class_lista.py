7189e9900a6a53ed2eb6956700544206b7715938
unknown <alineeuu@outlook.com>
2021-07-06 19:22:30 -0300

    esqueleto teste

---
+++ Class_lista.py
@@ -0,0 +1,22 @@
+class Lista:
+    def __init__(self):
+        pass
+
+    def iniciar(self):
+        pass
+
+    def inserir_pos_i(self):
+        pass
+
+    def remover_pos_i(self):
+        pass
+
+    def getPosicao(self):
+        pass
+
+    def limpar(self):
+        pass
+
+
+lista = Lista()
+
