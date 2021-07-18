0291a92697c6a3d78f4ef9bcdce890098de165e6
unknown <alineeuu@outlook.com>
2021-07-18 20:44:37 -0300

    Estrutura base

---
+++ Class_lista_encadeada.py
@@ -0,0 +1,24 @@
+class nodo:
+    def __init__(self, dado):
+        pass
+        self.dado = None
+        self.proximo_elemento = None
+
+class lista:
+    def __init__(self):
+        self.inicio = None
+
+    def insere(self, valor, posicao_item):
+        pass
+
+    def remove(self, posicao_item):
+        pass
+    
+    def posicao(self, valor):
+        pass
+
+    def valor(self, posicao_item):
+        pass
+    
+    def destroi(self):
+        pass
