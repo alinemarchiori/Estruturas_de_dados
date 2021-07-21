6a12f5c6c6c443cea090a7ae554ac194a68a188d
unknown <alineeuu@outlook.com>
2021-07-21 10:46:20 -0300

    operacoes basicas concluidas

--- Class_lista_encadeada.py
+++ Class_lista_encadeada.py
@@ -8,9 +8,9 @@ class Lista:
         self.inicio = None
         self.quantidade_itens = 0
 
-    def insere(self, valor, posicao_item=-1):
+    def insere(self, valor, posicao_item=-1): #funcao inserir
 
-        if self.inicio:
+        if self.inicio: #verifica se está vazia 
             auxiliar = self.inicio
 
             if posicao_item == -1: #insere no final
@@ -46,12 +46,10 @@ class Lista:
             self.quantidade_itens += 1
 
     def remove(self, posicao_item):
-        if self.inicio:
+        if self.inicio: #verifica se está vazia 
             auxiliar = self.inicio
-
             if posicao_item > 0 and posicao_item <= self.quantidade_itens:# remove do meio
-                for i in range(0, posicao_item-1):
-                    auxiliar = auxiliar.proximo_elemento
+                for i in range(0, posicao_item-1): auxiliar = auxiliar.proximo_elemento
                 remover = auxiliar.proximo_elemento
                 auxiliar.proximo_elemento = remover.proximo_elemento
                 self.quantidade_itens -= 1
@@ -61,9 +59,8 @@ class Lista:
             else: print("posicao invalida")
 
         else: print("lista vazia")
-
     
-    def posicao(self, valor):
+    def getPosicao(self, valor): #encontra a posicao baseado no valor
         contador = 0
         auxiliar = self.inicio
         for i in range(self.quantidade_itens):
@@ -73,29 +70,27 @@ class Lista:
         print("não encontrado")
         return None
 
-    def valor(self, posicao_item):
-        pass
+    def getValor(self, posicao_item): # encontra o valor baseado na posicao
+        auxiliar = self.inicio
+        for i in range(0, posicao_item): auxiliar = auxiliar.proximo_elemento
+        return auxiliar.dado
     
-    def destroi(self):
-        pass
+    def destroi(self): self.inicio = None; self.quantidade_itens = 0
 
     def mostra(self): # mostra elementos
+        if self.quantidade_itens == 0: print("lista vazia")
         objeto = self.inicio
         for i in range(self.quantidade_itens):
             print(objeto.dado, end=" ")
             objeto = objeto.proximo_elemento
         print()
-    
-    def vazia(self):
-        if self.quantidade_itens <= 0:
-            self.inicio = None
 
+#TESTES
 lista = Lista()
 lista.insere(0, 0)
 lista.insere(1)
 lista.insere(2)
 lista.insere(3)
-
 lista.insere(5)
 lista.insere(6)
 lista.mostra()
@@ -105,4 +100,7 @@ lista.insere(0, 4)
 lista.mostra()
 lista.remove(0)
 lista.mostra()
-print(lista.posicao(0))
+print(lista.getPosicao(0))
+print(lista.getValor(3))
+lista.destroi()
+lista.mostra()
