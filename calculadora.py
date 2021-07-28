c84c3c87f93677925cf818b1d381bcbae3f5cbc6
unknown <alineeuu@outlook.com>
2021-07-28 12:52:56 -0300

    funcao multiplicacao

--- calculadora.py
+++ calculadora.py
@@ -1,18 +1,49 @@
 class Calculadora:
     def __init__(self):
+        self.shift = 0
+        self.primeiro = self.segundo = []
+        self.matriz_soma = None
+
+    def soma(self, primeiro, segundo):
+        return primeiro + segundo
+
+    def multiplica(self, primeiro, segundo):
+        self.matriz_soma = [0] * (len(primeiro) + len(primeiro))
+        self.primeiro, self.segundo = primeiro, segundo
+        carry, self.shift , resto = 0, 0, 0
+        for i in range(len(primeiro)-1, 0, -1):
+            multiplicador, carry = int(primeiro[i]), 0
+            for i in range(len(segundo)-1, 0, -1):
+                multiplicando, result = int(segundo[i]), 0
+                for i in range(multiplicador):
+                    result = self.soma(result,multiplicando)
+                result = result + carry
+                if result > 9:
+                    result = list(str(result))
+                    resto = int(result[1])
+                    carry = int(result[0])
+                else:
+                    carry = 0
+
+    def addMatrizSoma(self, n):
         pass
 
-    def soma(self):
-        pass
+    def adicionaZeros(self, numero, tamanho):
+        lista = [0] * tamanho
+        numero = lista + numero
 
-    def multiplica(self):
-        pass
+    def normalizaTamanho(self):
+        if len(self.primeiro) > len(self.segundo):
+            self.segundo = self.adicionaZeros(self.segundo, len(self.primeiro)-len(self.segundo))
+
+                    
+
 
 
 def main():
     operacao = Calculadora()
     while True:
-        escolha = int(input("1 - Meltiplicacao;\n2 - Soma;\n3 - Sair."))
+        escolha = int(input("1 - Multiplicacao;\n2 - Soma;\n3 - Sair."))
         if escolha == 1:
             d1 = input("Digite o primeiro numero: ")
             d2 = input("Digite o segundo numero: ")
