a1b96b517e0ad56ab232fb0fd4000a3461397add
unknown <alineeuu@outlook.com>
2021-08-13 23:24:44 -0300

    fila

--- Class_fila_encadeada.py
+++ Class_fila_encadeada.py
@@ -7,27 +7,28 @@ class Noh:
 class PilhaEncadeada:
     def __init__(self):
         self.inicio = None
+        self.fim = None
         self.tamanho = 0
 
     def insere(self, dado):#insere no topo da pilha 
+
         if self.tamanho == 0:#verifica se está vazia
             self.inicio = Noh(dado)#insere o primeiro elemento
+            self.fim = self.inicio
             self.tamanho += 1
         
         else:
-            auxiliar = self.inicio
-            while auxiliar.proximo_elemento: auxiliar = auxiliar.proximo_elemento
-            auxiliar.proximo_elemento = Noh(dado) #insere no final
+            self.fim.proximo_elemento = Noh(dado) #insere no final
             self.tamanho += 1
+
+        self.fim = Noh(dado)
     
     def remove(self):# remove do topo da pilha
         if self.inicio:
             auxiliar = self.inicio
-            while auxiliar.proximo_elemento:
-                anterior = auxiliar
-                auxiliar = auxiliar.proximo_elemento
+            self.inicio = auxiliar.proximo_elemento
 
-            auxiliar.proximo_elemento = None
+            auxiliar.proximo_elemento == None
             anterior.proximo_elemento = None
             self.tamanho -= 1
 
